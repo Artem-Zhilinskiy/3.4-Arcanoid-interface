256b23c8e7ce39a468a291e35510741285cecf08
Artem-Zhilinskiy <90002515+Artem-Zhilinskiy@users.noreply.github.com>
2022-02-13 20:57:43 +0300

    13.02.2022
    Сделан переход между меню настроек

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public static string _activeScene;
     public void NewGame()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void Back()
+    {
+        Debug.Log(_activeScene);
+        SceneManager.LoadScene(_activeScene);
+    }
+
+    public void Preferences()
+    {
+        _activeScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("PreferencesScene");
+        Debug.Log(_activeScene);
     }
 }
